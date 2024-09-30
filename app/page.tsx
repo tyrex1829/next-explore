75390a23c2d56c15f2e157058eaab539622e7bf8
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-30 19:42:48 +0530

    ssr to ui from backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-import Image from "next/image";
+import axios from "axios";
 
-export default function Home() {
-  return <div>Hello There, Nextjs Tweeks.</div>;
+async function getUserDetails() {
+  const response = await axios.get(
+    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+  );
+  return response.data;
+}
+
+export default async function Home() {
+  const userData = await getUserDetails();
+
+  return (
+    <div>
+      Hi there
+      <h1>{userData.email}</h1>
+      <h1>{userData.name}</h1>
+    </div>
+  );
 }
