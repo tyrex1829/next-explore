6b9271eec4aa40bf822d44e7e4cbec4307d184b7
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-30 15:18:34 +0530

    client side button comp

--- components/signin.tsx
+++ components/signin.tsx
@@ -1,10 +1,6 @@
-"use client";
+import { Button } from "./Button";
 
 export const SigninComponent = () => {
-  function handler() {
-    console.log("on click event");
-  }
-
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -23,13 +19,7 @@ export const SigninComponent = () => {
                 type={"password"}
                 placeholder="123456"
               />
-              <button
-                onClick={handler}
-                type="button"
-                className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-              >
-                Sign in
-              </button>
+              <Button />
             </div>
           </div>
         </a>
