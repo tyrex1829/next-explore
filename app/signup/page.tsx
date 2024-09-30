537e8ffd15eed66405be78317f501abb75f58af3
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-30 13:18:59 +0530

    signup route through file based routing

---
+++ app/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function Signup() {
+  return (
+    <div>
+      <h1>Hello There, Nextjs Tweeks.</h1>
+      <h3>This is sign-up endpoint through file based routing.</h3>
+    </div>
+  );
+}
