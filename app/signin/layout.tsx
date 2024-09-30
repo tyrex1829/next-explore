ae63d54d63e2d3c18d2251d2fcecd052f725c60b
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-30 14:10:12 +0530

    discount banner at signin route

--- app/signin/layout.tsx
+++ app/signin/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <div className="border-b text-center">30% off for next 2 hrs.</div>
+      <div className="border-b  text-center">30% off for next 2 hrs.</div>
       {children}
     </div>
   );
