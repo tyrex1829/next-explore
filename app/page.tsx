0523cbaa315c81ab769f81bf6ce5c8ee7f48dd02
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-30 19:43:17 +0530

    prettify the ui of ssr

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,14 @@ export default async function Home() {
   const userData = await getUserDetails();
 
   return (
-    <div>
-      Hi there
-      <h1>{userData.email}</h1>
-      <h1>{userData.name}</h1>
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>Name: {userData?.name}</div>
+
+          {userData?.email}
+        </div>
+      </div>
     </div>
   );
 }
