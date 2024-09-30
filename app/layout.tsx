e939d40530e0b9e1994ad0be206285f7d2765277
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-30 13:58:16 +0530

    topbar in layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="p-4 border-b mb-4">Top-Bar Of Website</div>
         {children}
       </body>
     </html>
