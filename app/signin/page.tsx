6d4955f914de68eb0ac2209dac778c9f1cf5b5d5
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-30 13:20:21 +0530

    signin route through file based routing

---
+++ app/signin/page.tsx
@@ -0,0 +1,8 @@
+export default function Signin() {
+  return (
+    <div>
+      <h1>Hello There, Nextjs Tweeks.</h1>
+      <h3>This is sign-in endpoint through file based routing.</h3>
+    </div>
+  );
+}
