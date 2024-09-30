60eea7596ac5c9cf18dadc32a118565dff2cc677
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-30 19:51:39 +0530

    prettify loader code

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,7 @@
-export default function () {
-  return <div className="text-center">Loading...</div>;
+export default function Loading() {
+  return (
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">Loading...</div>
+    </div>
+  );
 }
