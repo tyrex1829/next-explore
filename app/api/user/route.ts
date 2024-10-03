9e8bc0134e5a2d5241086616534fa26bc05408b5
tyrex1829 <sakshamkhare19@gmail.com>
2024-10-03 13:37:46 +0530

    code update: get & post request in routes

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,21 @@
+import { NextRequest } from "next/server";
+
 export async function GET() {
   // DB Logic
   return Response.json({
     email: "shrimpson@xyz.com",
     name: "Spike",
+  });
+}
+
+export async function POST(req: NextRequest) {
+  // Extract the body
+  const body = await req.json();
+
+  // Store the body in the database
+  console.log(body);
+
+  return Response.json({
+    message: "You are logged in!",
   });
 }
