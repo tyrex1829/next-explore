cfb9ad53b26029e8847d9c633723082d1e1427ae
tyrex1829 <sakshamkhare19@gmail.com>
2024-10-03 16:30:05 +0530

    code update: use router hook for landing page redirection:

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -2,10 +2,13 @@
 
 import axios from "axios";
 import { ChangeEventHandler, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export function Signup() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const router = useRouter();
 
   return (
     <div className="h-screen flex justify-center flex-col">
@@ -43,6 +46,7 @@ export function Signup() {
                       password,
                     }
                   );
+                  router.push("/");
                 }}
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
