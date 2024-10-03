716273d4e3c8441e5a40a783be9fda6f8d06a9c5
tyrex1829 <sakshamkhare19@gmail.com>
2024-10-03 13:04:59 +0530

    code fix: signup component & signup route

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,11 +1,5 @@
-export default function Signup() {
-  return (
-    <div>
-      <input type="text" placeholder="Username" />
-      <br />
-      <input type="text" placeholder="Password" />
-      <br />
-      <button>Signup</button>
-    </div>
-  );
+import { Signup } from "@/components/Signup";
+
+export default function () {
+  return <Signup />;
 }
