56dbd620d0e0d1c4f92f414ec95b8ef350e09549
tyrex1829 <sakshamkhare19@gmail.com>
2024-10-03 13:18:46 +0530

    code update: signup component

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import axios from "axios";
 import { ChangeEventHandler, useState } from "react";
 
@@ -22,7 +24,7 @@ export function Signup() {
                   setUsername(e.target.value);
                 }}
                 label="Username"
-                placeholder="harkirat@gmail.com"
+                placeholder="user@xyz.com"
               />
               <LabelledInput
                 onChange={(e) => {
@@ -33,6 +35,15 @@ export function Signup() {
                 placeholder="123456"
               />
               <button
+                onClick={async () => {
+                  const response = await axios.post(
+                    "http://localhost:3000/api/user",
+                    {
+                      username,
+                      password,
+                    }
+                  );
+                }}
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
               >
